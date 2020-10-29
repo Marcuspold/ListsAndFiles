248b497388f48d9dbfedd3e3feff8e613250e542
Opilane TTHK <opilane@tthk.ee>
2020-10-29 11:37:28 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListsAndFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good Morning!");
+            List<string> fruit = new List<string>();
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter a fruit:");
+                fruit.Add(Console.ReadLine());
+            }
+            foreach(string fruititem in fruit)
+            {
+                Console.WriteLine($"Fruit on the list: {fruititem}");
+            }
+            fruit.Add("pineapple");
+            foreach (string fruititem in fruit)
+            {
+                Console.WriteLine($"Fruit on the list: {fruititem}");
+            }
+            int indexOfPineapple = fruit.IndexOf("pineapple");
+
+            Console.WriteLine($"Pineapple is on index {indexOfPineapple}");
+
+            fruit[indexOfPineapple] = "Orange";
+            foreach (string fruititem in fruit)
+            {
+                Console.WriteLine($"Fruit on Updated list: {fruititem}");
+            }
+
+
+            fruit.RemoveAt(fruit.Count-1);
+
+            foreach (string fruititem in fruit)
+            {
+                Console.WriteLine($"Trimed Fruit list: {fruititem}");
+            }
+        }
+    }
+}
